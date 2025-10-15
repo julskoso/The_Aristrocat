b709ba0de740ff47f732c053f5e4c698477700d5
julskoso <julski707@gmail.com>
2025-10-15 23:02:51 +0200

    Add free-fly camera pawn

---
+++ The_Aristrocat/Source/The_Aristrocat/FreeFlyCameraGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FreeFlyCameraGameMode.generated.h"
+
+UCLASS()
+class THE_ARISTROCAT_API AFreeFlyCameraGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AFreeFlyCameraGameMode();
+};
